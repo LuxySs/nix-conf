acdde601525e02d9742a2e429d815aaaaf707859
lulu <lucas.verbeiren@gmail.com>
2024-06-07 23:09:42 +0200

    trying to use git on host machine

---
+++ home-manager/home.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }: {
+  home = {
+    username = "lulu";
+    homeDirectory = "/home/lulu";
+    stateVersion = "24.05";
+  };
+
+  programs.neovim = {
+    enable = true;
+  };
+
+  programs.foot = {
+    enable = true;
+  };
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      coucou_test = "echo coucou";
+    };
+  };
+}
