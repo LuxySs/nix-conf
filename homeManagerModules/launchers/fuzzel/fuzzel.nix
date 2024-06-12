f9c0335dd3ebee40fcf2587e813ea70d05e2e51c
lulu <lucas.verbeiren@gmail.com>
2024-06-12 22:14:59 +0200

    fuzzel + nano config

--- homeManagerModules/launchers/fuzzel/fuzzel.nix
+++ homeManagerModules/launchers/fuzzel/fuzzel.nix
@@ -12,5 +12,28 @@ in
 
   config = mkIf cfg.enable {
     programs.fuzzel.enable = true;
+    programs.fuzzel.settings = {
+      main = {
+        terminal = "${pkgs.foot}/bin/foot";
+        layer = "overlay";
+        prompt = "❯ ";
+        lines = 10;
+        line-height = 20;
+        "font=Dank Mono:weight = bold:size" = 15;
+      };
+      colors = {
+        background = "24273add";
+        text = "cad3f5ff";
+        match = "ed8796ff";
+        selection = "5b6078ff";
+        selection-match = "ed8796ff";
+        selection-text = "cad3f5ff";
+        border = "51A4E7FF";
+      } ;
+      border = {
+        radius = "20";
+        width = "2";
+      };
+    };
   };
 }
--- homeManagerModules/terminals/foot/foot.nix
+++ homeManagerModules/terminals/foot/foot.nix
@@ -12,5 +12,18 @@ in
 
   config = mkIf cfg.enable {
     programs.foot.enable = true;
+    programs.foot.settings = {
+      main = {
+        term = "xterm-256color";
+        font = "font=Iosevka Nerd Font:size=13";
+        dpi-aware = "yes";
+      };
+      mouse = {
+        hide-when-typing = "yes";
+      };
+      cursor = {
+        color = "000000 ffffff";
+      };
+    };
   };
 }
