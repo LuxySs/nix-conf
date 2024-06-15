d3f4e5e3a4bc5600ad07dd8b91ec85d6fb0ac7cd
lulu <lucas.verbeiren@gmail.com>
2024-06-15 23:16:04 +0200

    starship theming with stylix

--- homeManagerModules/features/prompts/starship/starship.nix
+++ homeManagerModules/features/prompts/starship/starship.nix
@@ -4,6 +4,22 @@ with lib;
 
 let
   cfg = config.starshipModule;
+  colors = config.lib.stylix.colors;
+  base01 = colors.base01;
+  base02 = colors.base02;
+  base03 = colors.base03;
+  base04 = colors.base04;
+  base05 = colors.base05;
+  base06 = colors.base06;
+  base07 = colors.base07;
+  base08 = colors.base08;
+  base09 = colors.base09;
+  base0A = colors.base0A;
+  base0B = colors.base0B;
+  base0C = colors.base0C;
+  base0D = colors.base0D;
+  base0E = colors.base0E;
+  base0F = colors.base0F;
 in
 {
   options.starshipModule = {
@@ -17,27 +33,27 @@ in
         command_timeout = 3000;
 
         format = 
-          "[░▒▓](#333333)" +
-          "[ ](bg:#333333 fg:#1793d1)" +
-          "[](bg:#333333 fg:#333333)" +
+          "[░▒▓](#${base02})" +
+          "[ ](bg:#${base02} fg:#${base0E})" +
+          "[](bg:#${base02} fg:#${base02})" +
           "$directory" +
-          "[](fg:#333333 bg:#333333)" +
+          "[](fg:#${base02} bg:#${base02})" +
           "$git_branch" +
           "$git_status" +
-          "[](fg:#333333 bg:#333333)" +
+          "[](fg:#${base02} bg:#${base02})" +
           "$nodejs" +
           "$cmd_duration" +
-          "[](fg:#333333 bg:#333333)" +
+          "[](fg:#${base02} bg:#${base02})" +
           "$python" +
-          "[ ](fg:#333333)" +
+          "[ ](fg:#${base02})" +
           "\n" +
-          "[ ❯](fg:#00bdff)" +
-          "[❯](fg:#9d91a2)" +
-          "[❯ ](fg:#ee0000)"
+          "[ ❯](fg:#${base0B})" +
+          "[❯](fg:#${base0C})" +
+          "[❯ ](fg:#${base0D})"
         ;
 
         directory = {
-          style = "fg:#e3e5e5 bg:#333333";
+          style = "fg:#${base05} bg:#${base02}";
           format = "[ $path ]($style)";
           substitutions = {
             "~/Documents" = "󰈙 ";
@@ -51,49 +67,49 @@ in
 
         git_branch = {
           symbol = "";
-          style = "bg:#394260";
-          format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
+          style = "bg:#${base04}";
+          format = "[[ $symbol $branch ](fg:#${base0D} bg:#${base03})]($style)";
         };
 
         git_status = {
-          style = "bg:#394260";
-          format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
+          style = "bg:#${base04}";
+          format = "[[($all_status$ahead_behind )](fg:#${base0D} bg:#${base03})]($style)";
         };
 
         nodejs = {
           symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+          style = "bg:#${base0D}";
+          format = "[[ $symbol ($version) ](fg:#${base0D} bg:#212736)]($style)";
         };
 
         rust = {
           symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+          style = "bg:#${base0D}";
+          format = "[[ $symbol ($version) ](fg:#${base0D} bg:#212736)]($style)";
         };
 
         golang = {
           symbol = "ﳑ";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+          style = "bg:#${base0D}";
+          format = "[[ $symbol ($version) ](fg:#${base0D} bg:#${base02})]($style)";
         };
 
         php = {
           symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+          style = "bg:#${base0D}";
+          format = "[[ $symbol ($version) ](fg:#${base0D} bg:#${base02})]($style)";
         };
 
         python = {
           symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#1d2230)]($style)";
+          style = "bg:#${base0D}";
+          format = "[[ $symbol ($version) ](fg:#${base0D} bg:#${base02})]($style)";
         };
 
         cmd_duration = {
           disabled = false;
-          style = "bg:#1d2230";
-          format = "[[  $duration ](fg:#769ff0 bg:#212736)]($style)";
+          style = "bg:#${base0D}";
+          format = "[[  $duration ](fg:#${base0D} bg:#${base02})]($style)";
         };
       };
     };
