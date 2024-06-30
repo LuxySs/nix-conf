9c0a668ffa91df702f7748bad1b997bfe383a520
lulu <lucas.verbeiren@gmail.com>
2024-07-01 00:06:30 +0200

    sddm theming

--- nixosModules/sddm.nix
+++ nixosModules/sddm.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   options.settings.sddm.enable = lib.mkOption {
@@ -10,6 +10,16 @@
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
+      theme = "catppuccin-mocha";
+      package = pkgs.kdePackages.sddm;
     };
+
+    environment.systemPackages = [(
+      pkgs.catppuccin-sddm.override {
+        flavor = "mocha";
+        fontSize = "13";
+        loginBackground = true;
+      }
+    )];
   };
 }
