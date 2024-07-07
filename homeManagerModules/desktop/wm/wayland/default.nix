8eea85728491fb98484a434f80965674099ea62f
lulu <lucas.verbeiren@gmail.com>
2024-07-07 10:59:15 +0200

    got hypridle working

--- homeManagerModules/desktop/wm/wayland/default.nix
+++ homeManagerModules/desktop/wm/wayland/default.nix
@@ -5,6 +5,7 @@
     ./brightness.nix
     ./fuzzel.nix
     ./hyprlock.nix
+    ./hypridle.nix
     ./screenshots.nix
     ./swww.nix
     ./waybar.nix
---
+++ homeManagerModules/desktop/wm/wayland/hypridle.nix
@@ -0,0 +1,33 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.settings.wm.hypridle;
+in
+{
+  options.settings.wm.hypridle.enable = lib.mkDisableOption "hypridle";
+
+  config = lib.mkIf (cfg.enable) {
+    services.hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          before_sleep_cmd = "loginctl lock-session";
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+          lock_cmd = "pidof hyprlock || hyprlock";
+        };
+
+        listener = [
+          {
+            timeout = 30;
+            on-timeout = "hyprlock";
+          }
+          {
+            timeout = 60;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+        ];
+      };
+    };
+  };
+}
