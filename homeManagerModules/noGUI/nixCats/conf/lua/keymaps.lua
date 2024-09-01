0f17fe97e6c3550b3dde0b0280f915f3bf3097d0
lulu <lucas.verbeiren@gmail.com>
2024-09-01 21:58:08 +0200

    removed a garbage shortcut

--- homeManagerModules/noGUI/nixCats/conf/lua/keymaps.lua
+++ homeManagerModules/noGUI/nixCats/conf/lua/keymaps.lua
@@ -5,9 +5,6 @@ local map = vim.keymap.set
 
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Basic functionality
-map('n', 'vl', 'vg_', { desc = 'Select to end of Line' })
-
 -- Diagnostic keymaps
 map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 map('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
