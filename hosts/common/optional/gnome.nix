459361a3d8e73a85bd347688605950cb92f8e221
lulu <lucas.verbeiren@gmail.com>
2024-09-06 17:18:37 +0200

    added bloated option for kde (disable useless programs)

--- hosts/common/optional/gnome.nix
+++ hosts/common/optional/gnome.nix
@@ -11,50 +11,48 @@ in
 {
   options.settings.gnome = {
     enable = lib.mkEnableOption "GNOME DE";
-    bloated = lib.mkDefault false;
+    bloated = lib.mkEnableOption "Bloated GNOME Install (all the apps)";
   };
 
   config = lib.mkIf (cfg.enable) {
     services.xserver.desktopManager.gnome.enable = true;
 
-    # disable all the "useless" packages unless we enabled "bloated"
-    config = lib.mkIf (!cfg.bloated) {
-      environment.gnome.excludePackages = (
-        with pkgs;
-        [
-          atomix # Puzzle Game
-          baobab # Disk Usage Analyzer
-          cheese # Webcam Tool
-          epiphany # Web Browser
-          evince # Document Viewer
-          file-roller # (Archive Manager)
-          geary # Email Reader
-          gedit # Text Editor
-          gnome-characters
-          gnome-clocks # Clocks
-          gnome-color-manager # Color Profile Viewer
-          gnome-connections # Connections
-          gnome-contacts # Contacts
-          gnome-disk-utility # Disk Usage Analyzer
-          gnome-font-viewer # Fonts
-          gnome-maps # Maps
-          gnome-music
-          gnome-photos
-          gnome-system-monitor # System Monitor
-          gnome-terminal # Trash terminal (not the console one)
-          gnome-text-editor # Text Editor
-          gnome-tour # A guided tour and greeter for GNOME
-          gnome-weather # Weather
-          hitori # Sudoku Game
-          iagno # Go Game
-          seahorse # Passwords and Keys
-          simple-scan # Document Scanner
-          tali # Poker Game
-          totem # Video Player
-        ]
-      );
-      services.gnome.rygel.enable = lib.mkDefault false;
-    };
+    # Exclude the useless apps unless "bloated" was enabled
+    services.gnome.rygel.enable = cfg.bloated;
+    environment.gnome.excludePackages = lib.mkIf (!cfg.bloated) (
+      with pkgs;
+      [
+        atomix # Puzzle Game
+        baobab # Disk Usage Analyzer
+        cheese # Webcam Tool
+        epiphany # Web Browser
+        evince # Document Viewer
+        file-roller # Archive Manager
+        geary # Email Reader
+        gedit # Text Editor
+        gnome-characters
+        gnome-clocks # Clocks
+        gnome-color-manager # Color Profile Viewer
+        gnome-connections # Connections
+        gnome-contacts # Contacts
+        gnome-disk-utility # Disk Usage Analyzer
+        gnome-font-viewer # Fonts
+        gnome-maps # Maps
+        gnome-music
+        gnome-photos
+        gnome-system-monitor # System Monitor
+        gnome-terminal # Trash terminal (not the console one)
+        gnome-text-editor # Text Editor
+        gnome-tour # A guided tour and greeter for GNOME
+        gnome-weather # Weather
+        hitori # Sudoku Game
+        iagno # Go Game
+        seahorse # Passwords and Keys
+        simple-scan # Document Scanner
+        tali # Poker Game
+        totem # Video Player
+      ]
+    );
 
     # use pipewire instead of pulseaudio
     hardware.pulseaudio.enable = lib.mkForce false;
--- hosts/common/optional/kde.nix
+++ hosts/common/optional/kde.nix
@@ -1,10 +1,36 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   cfg = config.settings.kde;
 in
 {
-  options.settings.kde.enable = lib.mkEnableOption "KDE DE";
+  options.settings.kde = {
+    enable = lib.mkEnableOption "KDE DE";
+    bloated = lib.mkEnableOption "Bloated GNOME Install (all the apps)";
+  };
 
-  config = lib.mkIf (cfg.enable) { services.desktopManager.plasma6.enable = true; };
+  config = lib.mkIf (cfg.enable) {
+    services.desktopManager.plasma6.enable = true;
+
+    # Exclude the useless apps unless "bloated" was enabled
+    environment.plasma6.excludePackages = lib.mkIf (!cfg.bloated) (
+      with pkgs.kdePackages;
+      [
+        ark # Archive Manager
+        elisa # Music Player
+        gwenview # Image Viewer
+        kate # Text Editor
+        krdp # Remote Desktop
+        kwallet # Kwallet
+        okular # PDF Viewer
+        plasma-disks # Disks Viewer
+        spectacle # Screenshotting tool
+      ]
+    );
+  };
 }
