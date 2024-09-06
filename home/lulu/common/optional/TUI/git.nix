26819c22c38312b025056577a6f381d0ee2fc03c
lulu <lucas.verbeiren@gmail.com>
2024-09-06 11:51:29 +0200

    added git aliases

--- home/lulu/common/optional/TUI/git.nix
+++ home/lulu/common/optional/TUI/git.nix
@@ -11,6 +11,10 @@ in
       enable = true;
       userName = "lulu";
       userEmail = "lucas.verbeiren@gmail.com";
+      aliases = {
+        lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+        lg1 = "log --oneline --abbrev-commit --all --graph --decorate --color";
+      };
     };
   };
 }
