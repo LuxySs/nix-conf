f8ba3498654ee8480e918f09c17b29084c42179a
lulu <lucas.verbeiren@gmail.com>
2024-10-29 14:19:49 +0100

    nvim-ufo: treesitter instead of lsp to fix clangd
    When I was copying some code into the editor I had errors because of
    folds. Using treesitter instead of lsp for folds fixes this issue.

--- home/lulu/common/optional/terminal/nixCats/conf/lua/plugins/utils/ufo.lua
+++ home/lulu/common/optional/terminal/nixCats/conf/lua/plugins/utils/ufo.lua
@@ -21,7 +21,7 @@ return {
 
     require('ufo').setup {
       provider_selector = function(bufnr, filetype, buftype)
-        return { 'lsp', 'indent' }
+        return { 'treesitter', 'indent' }
       end,
     }
   end,
