b96564a8f65e439e8d01438198c1a216300abe34
lulu <lucas.verbeiren@gmail.com>
2024-10-30 18:40:14 +0100

    tmux keymaps

--- home/lulu/common/optional/terminal/multiplexers/tmux.nix
+++ home/lulu/common/optional/terminal/multiplexers/tmux.nix
@@ -23,12 +23,19 @@ in
         set -sg escape-time 10
 
         setw -g mode-keys vi
+
+        bind-key -T copy-mode-vi v send-keys -X begin-selection
+        bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+        bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
       '';
 
       # vim-like navigation
       customPaneNavigationAndResize = true;
 
-      plugins = with pkgs; [ tmuxPlugins.vim-tmux-navigator ];
+      plugins = with pkgs.tmuxPlugins; [
+        vim-tmux-navigator
+        yank
+      ];
 
       shell = if fish_enabled then "${pkgs.fish}/bin/fish" else null;
     };
