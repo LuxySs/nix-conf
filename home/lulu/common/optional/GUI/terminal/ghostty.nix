d795d700e8fba5953274806d9ac4ac5910292e0a
lulu <lucas.verbeiren@gmail.com>
2024-12-30 16:30:43 +0100

    added ghostty terminal option, very slow to open so not using it yet

---
+++ home/lulu/common/optional/GUI/terminal/ghostty.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.settings.ghostty;
+in
+{
+  options.settings.ghostty.enable = lib.mkEnableOption "ghostty";
+
+  config = lib.mkIf (cfg.enable) {
+    programs.ghostty = {
+      enable = true;
+      enableFishIntegration = true;
+      enableBashIntegration = true;
+
+      settings = {
+        theme = "catppuccin-mocha";
+
+        cursor-color = "ffffff";
+        cursor-style = "block";
+        cursor-style-blink = false;
+
+        font-family = "Iosevka Nerd Font";
+        font-size = 17;
+
+        shell-integration-features = "no-cursor";
+
+        confirm-close-surface = false;
+        window-decoration = false;
+      };
+    };
+  };
+}
