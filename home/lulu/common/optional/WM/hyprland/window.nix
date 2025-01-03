7f6ad2d118455782b9afef8cb47275391ff84164
lulu <lucas.verbeiren@gmail.com>
2025-01-03 19:49:28 +0100

    hyprland smart gaps

--- home/lulu/common/optional/WM/hyprland/window.nix
+++ home/lulu/common/optional/WM/hyprland/window.nix
@@ -7,6 +7,19 @@
       gaps_out = 7;
       border_size = 2;
       layout = "dwindle";
+
+      workspace = [
+        "w[tv1], gapsout:0, gapsin:0"
+        "f[1], gapsout:0, gapsin:0"
+      ];
+
+      # this allows for smart gaps (no gaps when one single window)
+      windowrulev2 = [
+        "bordersize 0, floating:0, onworkspace:w[tv1]"
+        "rounding 0, floating:0, onworkspace:w[tv1]"
+        "bordersize 0, floating:0, onworkspace:f[1]"
+        "rounding 0, floating:0, onworkspace:f[1]"
+      ];
     };
   };
 }
