e254f9a42a84d18a0997060842ae7daa4c3aeb1d
lulu <lucas.verbeiren@gmail.com>
2025-01-18 19:00:05 +0100

    added linting and formatting

---
+++ home/lulu/common/optional/terminal/nixCats/conf/lua/plugins/programming/lsp/lsp-config.lua
@@ -0,0 +1,159 @@
+return {
+  'neovim/nvim-lspconfig',
+  enabled = require('nixCatsUtils').enableForCategory{'programming', 'lsp'} ,
+  event = { 'BufReadPre', 'BufNewFile' },
+  dependencies = {
+    -- Useful status updates for LSP.
+    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+    { 'j-hui/fidget.nvim', opts = {} },
+
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim apis
+    {
+      'folke/lazydev.nvim',
+      ft = 'lua',
+      opts = {
+        library = {
+          -- adds type hints for nixCats global
+          -- THIS LINE FUCK UP IF YOU ARE NOT USING NIX
+          -- { path = require('nixCats').nixCatsPath .. '/lua', words = { 'nixCats' } },
+        },
+      },
+    },
+  },
+  opts = {
+    servers = {
+      bashls = {},
+      clangd = {},
+      gopls = {},
+      neocmake = {},
+      pyright = {},
+      rust_analyzer = {},
+      ts_ls = {},
+      lua_ls = {
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = 'Replace',
+            },
+            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+            diagnostics = {
+              globals = { 'nixCats' },
+              disable = { 'missing-fields' },
+            },
+          },
+        },
+      }
+    },
+  },
+  config = function(_, opts)
+    local lspconfig = require('lspconfig')
+    for server, config in pairs(opts.servers) do
+        -- passing config.capabilities to blink.cmp merges with the capabilities in your
+        -- `opts[server].capabilities, if you've defined it
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+    end
+
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+      callback = function(event)
+        -- NOTE: Remember that Lua is a real programming language, and as such it is possible
+        -- to define small helper and utility functions so you don't have to repeat yourself.
+        --
+        -- In this case, we create a function that lets us more easily define mappings specific
+        -- for LSP related items. It sets the mode, buffer and description for us each time.
+        local map = function(keys, func, desc, mode)
+           mode = mode or 'n'
+           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+         end
+
+        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+
+        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+
+        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+        map('K', vim.lsp.buf.hover, 'Hover Documentation')
+
+        -- The following two autocommands are used to highlight references of the
+        -- word under your cursor when your cursor rests there for a little while.
+        --    See `:help CursorHold` for information about when this is executed
+        --
+        -- When you move your cursor, the highlights will be cleared (the second autocommand).
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+          local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.document_highlight,
+          })
+
+          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.clear_references,
+          })
+
+          vim.api.nvim_create_autocmd('LspDetach', {
+            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+            callback = function(event2)
+              vim.lsp.buf.clear_references()
+              vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+            end,
+          })
+        end
+
+        -- The following code creates a keymap to toggle inlay hints in your
+        -- code, if the language server you are using supports them
+        --
+        -- This may be unwanted, since they displace some of your code
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+          map('<leader>th', function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+          end, '[T]oggle Inlay [H]ints')
+        end
+      end,
+    })
+
+    -- LSP servers and clients are able to communicate to each other what features they support.
+    --  By default, Neovim doesn't support everything that is in the LSP specification.
+    --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+    --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+    -- NOTE: nixCats: nixd is not available on mason -> replace it with rnix + nil_ls.
+    if require('nixCatsUtils').isNixCats then
+      opts.servers.nixd = {}
+    else
+      servers.rnix = {}
+      servers.nil_ls = {}
+    end
+    -- NOTE: nixCats: if nix, use lspconfig instead of mason
+    if require('nixCatsUtils').isNixCats then
+      for server_name, _ in pairs(servers) do
+        require('lspconfig')[server_name].setup {
+          capabilities = capabilities,
+          settings = servers[server_name],
+          filetypes = (servers[server_name] or {}).filetypes,
+          cmd = (servers[server_name] or {}).cmd,
+          root_pattern = (servers[server_name] or {}).root_pattern,
+        }
+      end
+    end
+  end,
+}
