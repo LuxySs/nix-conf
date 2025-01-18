5deb038f9e7d4e16cf213181e73dca1de0d2f9c4
lulu <lucas.verbeiren@gmail.com>
2025-01-18 22:02:58 +0100

    blink-cmp improvements

--- home/lulu/common/optional/terminal/nixCats/conf/lua/plugins/completion/blink.lua
+++ home/lulu/common/optional/terminal/nixCats/conf/lua/plugins/completion/blink.lua
@@ -4,21 +4,23 @@ return {
   dependencies = 'rafamadriz/friendly-snippets',
 
   opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = 'default' },
-
-    fuzzy = {
-      prebuilt_binaries = {
-        download = false,
-      },
+    keymap = {
+      preset = 'default',
+      ['<C-l>'] = { 'snippet_forward', 'fallback' },
+      ['<C-h>'] = { 'snippet_backward', 'fallback' },
     },
 
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = 'normal',
+    },
+
+    signature = { enabled = true },
+
+    fuzzy = {
+      prebuilt_binaries = {
+        download = false,
+      },
     },
 
     sources = {
