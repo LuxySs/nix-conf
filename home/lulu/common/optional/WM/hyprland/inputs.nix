f057fb7cc392f5bbee39f7b7018caa97e097bb15
lulu <lucas.verbeiren@gmail.com>
2025-07-01 17:00:12 +0200

    laptop touchpad sensitivity

--- home/lulu/common/optional/WM/hyprland/inputs.nix
+++ home/lulu/common/optional/WM/hyprland/inputs.nix
@@ -7,12 +7,16 @@
       kb_variant = "intl";
       kb_options = "caps:swapescape";
 
-      sensitivity = -0.4;
       accel_profile = "flat";
-
       repeat_delay = 300;
       repeat_rate = 50;
-
     };
+
+    device = [
+      {
+        name = "syna8018:00-06cb:ce67-touchpad";
+        sensitivity = 1;
+      }
+    ];
   };
 }
