5254902c9fcfdaca1917459ceb51f3be40c09864
lulu <lucas.verbeiren@gmail.com>
2025-07-16 18:22:39 +0200

    shell common aliases

--- home/lulu/common/optional/shells/common-aliases.nix
+++ home/lulu/common/optional/shells/common-aliases.nix
@@ -1,28 +1,30 @@
 { config, lib, ... }:
 
 let
-  eza_enabled = config.settings.eza.enable;
-  eza_ls_replacement = {
+  baseAliases = {
+    ".." = "cd ..";
+    "..." = "cd ../../";
+    "...." = "cd ../../../";
+  };
+
+  ezaEnabled = config.settings.eza.enable;
+  ezaAliases = {
     ls = "eza --color=always --group-directories-first";
     la = "eza -a --color=always --group-directories-first";
     ll = "eza -l --color=always --group-directories-first";
     lla = "eza -la --color=always --group-directories-first";
     lt = "eza -aT --color=always --group-directories-first";
-
-    "l." = "eza -al --color=always --group-directories-first ../";
-    "l.." = "eza -al --color=always --group-directories-first ../../";
-    "l..." = "eza -al --color=always --group-directories-first ../../../";
   };
 
-  baseAliases = {
-    ".." = "cd ..";
-    "..." = "cd ../../";
-    "...." = "cd ../../../";
+  neovideEnabled = config.settings.neovide.enable;
+  neovideAliases = {
     ng = "neovide & disown";
   };
+
 in
 
 lib.mkMerge [
   baseAliases
-  (lib.mkIf eza_enabled eza_ls_replacement)
+  (lib.mkIf ezaEnabled ezaAliases)
+  (lib.mkIf neovideEnabled neovideAliases)
 ]
