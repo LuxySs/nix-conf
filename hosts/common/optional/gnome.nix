e9e173206b341b45223786cbb1737b06bf919a5a
lulu <lucas.verbeiren@gmail.com>
2025-10-11 10:27:40 +0200

    minor change gnome

--- hosts/common/optional/gnome.nix
+++ hosts/common/optional/gnome.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 
@@ -11,48 +10,20 @@ in
 {
   options.settings.gnome = {
     enable = lib.mkEnableOption "GNOME DE";
-    bloated = lib.mkEnableOption "Bloated GNOME Install (all the apps)";
+    core-apps.enable = lib.mkEnableOption "GNOME core apps";
+    core-developer-tools.enable = lib.mkEnableOption "GNOME core developer tools";
+    games.enable = lib.mkEnableOption "GNOME games";
   };
 
   config = lib.mkIf (cfg.enable) {
     services.desktopManager.gnome.enable = true;
+    services.gnome = {
+      core-os-services.enable = true;
+      core-shell.enable = true;
 
-    # Exclude the useless apps unless "bloated" was enabled
-    services.gnome.rygel.enable = cfg.bloated;
-    environment.systemPackages = with pkgs; [ gnome-tweaks ];
-    environment.gnome.excludePackages = lib.mkIf (!cfg.bloated) (
-      with pkgs;
-      [
-        atomix # Puzzle Game
-        baobab # Disk Usage Analyzer
-        cheese # Webcam Tool
-        epiphany # Web Browser
-        evince # Document Viewer
-        file-roller # Archive Manager
-        geary # Email Reader
-        gedit # Text Editor
-        gnome-characters
-        gnome-clocks # Clocks
-        gnome-color-manager # Color Profile Viewer
-        gnome-connections # Connections
-        gnome-contacts # Contacts
-        gnome-disk-utility # Disk Usage Analyzer
-        gnome-font-viewer # Fonts
-        gnome-maps # Maps
-        gnome-music
-        gnome-photos
-        gnome-system-monitor # System Monitor
-        gnome-terminal # Trash terminal (not the console one)
-        gnome-text-editor # Text Editor
-        gnome-tour # A guided tour and greeter for GNOME
-        gnome-weather # Weather
-        hitori # Sudoku Game
-        iagno # Go Game
-        seahorse # Passwords and Keys
-        simple-scan # Document Scanner
-        tali # Poker Game
-        totem # Video Player
-      ]
-    );
+      core-apps.enable = cfg.core-apps.enable;
+      core-developer-tools.enable = cfg.core-developer-tools.enable;
+      games.enable = cfg.games.enable;
+    };
   };
 }
