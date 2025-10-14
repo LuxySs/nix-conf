bdfd84b4b50ac83011693dcd4c1c787fc4a12f49
lulu <lucas.verbeiren@gmail.com>
2025-10-14 22:39:36 +0200

    vague colorscheme changes

--- pkgs/vague/default.nix
+++ pkgs/vague/default.nix
@@ -8,21 +8,21 @@ writeTextFile {
     name: "Vague"
     variant: "dark"
     palette:
-      base00: "#141415" # base (bg)
-      base01: "#1c1c24" # mantle (inactiveBg)
-      base02: "#252530" # surface0 (line)
-      base03: "#333738" # surface1 (visual)
-      base04: "#606079" # surface2 (comment)
-      base05: "#cdcdcd" # text (fg)
-      base06: "#b4d4cf" # accent1 (builtin)
-      base07: "#c3c3d5" # accent2 (property)
-      base08: "#d8647e" # red (error)
-      base09: "#e0a363" # orange (number)
-      base0A: "#f3be7c" # yellow (warning/delta)
-      base0B: "#7fa563" # green (plus)
-      base0C: "#b4d4cf" # cyan (builtin/teal tone)
-      base0D: "#7e98e8" # blue (hint)
-      base0E: "#6e94b2" # magenta (keyword/type)
-      base0F: "#e8b589" # brown (string)
+      base00: "#141415"
+      base01: "#1c1c24"
+      base02: "#252530"
+      base03: "#333738"
+      base04: "#878787"
+      base05: "#cdcdcd"
+      base06: "#c3c3d5"
+      base07: "#aeaed1"
+      base08: "#d8647e" # red
+      base09: "#e0a363" # orange
+      base0A: "#f3be7c" # yellow (orange2)
+      base0B: "#7fa563" # green
+      base0C: "#6e94b2" # cyan
+      base0D: "#7e98e8" # blue
+      base0E: "#bb9dbd" # purple
+      base0F: "#c48282" # brown
   '';
 }
