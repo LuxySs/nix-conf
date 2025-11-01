80462ba79993616c8444b7c6c33caec4c60231ed
lulu <lucas.verbeiren@gmail.com>
2025-11-01 18:02:09 +0100

    git module updated according to hm-module changes

--- home/lulu/common/optional/git.nix
+++ home/lulu/common/optional/git.nix
@@ -9,12 +9,17 @@ in
   config = lib.mkIf (cfg.enable) {
     programs.git = {
       enable = true;
-      userName = "lulu";
-      userEmail = "lucas.verbeiren@gmail.com";
 
-      aliases = {
-        lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
-        lg1 = "log --oneline --abbrev-commit --all --graph --decorate --color";
+      settings = {
+        user = {
+          name = "lulu";
+          email = "lucas.verbeiren@gmail.com";
+        };
+
+        alias = {
+          lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+          lg1 = "log --oneline --abbrev-commit --all --graph --decorate --color";
+        };
       };
 
       ignores = [
