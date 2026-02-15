bd5a0dab82a234b172199d58bdab9cf1dc9ef9ba
lulu <lucas.verbeiren@gmail.com>
2026-02-15 21:16:04 +0100

    virtualization: virtualbox
    I would much prefer to continue using virt-manager + KVM but my sysadmin
    course requires me to use virtualbox

--- hosts/common/optional/virtualization.nix
+++ hosts/common/optional/virtualization.nix
@@ -7,9 +7,18 @@ in
   options.settings.virtualization.enable = lib.mkEnableOption "virtualization";
 
   config = lib.mkIf (cfg.enable) {
-    virtualisation.libvirtd.enable = true;
-    programs.virt-manager.enable = true;
-    users.groups.libvirtd.members = [ "lulu" ];
+    # **** virt-manager ****
+    #
+    # virtualisation.libvirtd.enable = true;
+    # programs.virt-manager.enable = true;
+    # users.groups.libvirtd.members = [ "lulu" ];
     # virtualisation.spiceUSBRedirection.enable = true;
+
+    # **** virtualbox ****
+    virtualisation.virtualbox.host.enable = true;
+    users.extraGroups.vboxusers.members = [ "lulu" ];
+
+    # Guest Additions
+    virtualisation.virtualbox.guest.enable = true;
   };
 }
