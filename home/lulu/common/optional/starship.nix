2e8987d8d8b0e48a4ddb7fbc0445755554767a17
lulu <lucas.verbeiren@gmail.com>
2026-04-14 11:27:07 +0200

    default to gruvbox colors

--- home/lulu/common/optional/starship.nix
+++ home/lulu/common/optional/starship.nix
@@ -9,18 +9,22 @@ let
       config.lib.stylix.colors
     else
       {
-        base00 = "#282c34";
-        base01 = "#3a3f58";
-        base02 = "#4e5266";
-        base03 = "#5a5f7a";
-        base04 = "#6c71c4";
-        base05 = "#93a1a1";
-        base0A = "#268bd2";
-        base0B = "#2aa198";
-        base0C = "#b58900";
-        base0D = "#6c71c4";
-        base0E = "#d33682";
-        base08 = "#dc322f";
+        base00 = "#282828";
+        base01 = "#3c3836";
+        base02 = "#504945";
+        base03 = "#665c54";
+        base04 = "#bdae93";
+        base05 = "#d5c4a1";
+        base06 = "#ebdbb2";
+        base07 = "#fbf1c7";
+        base08 = "#fb4934";
+        base09 = "#fe8019";
+        base0A = "#fabd2f";
+        base0B = "#b8bb26";
+        base0C = "#8ec07c";
+        base0D = "#83a598";
+        base0E = "#d3869b";
+        base0F = "#d65d0e";
       };
 
   mkFg = fgColor: "fg:#${fgColor}";
