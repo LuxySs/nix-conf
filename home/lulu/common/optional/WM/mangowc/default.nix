80fbdfff86db1cee80bc4562c3b55c3a48506b4c
lulu <lucas.verbeiren@gmail.com>
2026-04-14 12:53:05 +0200

    disabled mango hot area

--- home/lulu/common/optional/WM/mangowc/default.nix
+++ home/lulu/common/optional/WM/mangowc/default.nix
@@ -127,7 +127,7 @@ in
 
         # Overview Setting
         hotarea_size=10
-        enable_hotarea=1
+        enable_hotarea=0
         ov_tab_mode=0
         overviewgappi=5
         overviewgappo=30
